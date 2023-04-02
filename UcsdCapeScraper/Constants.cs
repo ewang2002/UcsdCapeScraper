fb2dd724d9146c2d890dcb313087362da5cd43c9
ewang2002 <37031713+ewang2002@users.noreply.github.com>
2023-04-02 00:59:14 -0700

    feat: scrape CAPE data for courses not under dept list

--- UcsdCapeScraper/Constants.cs
+++ UcsdCapeScraper/Constants.cs
@@ -5,11 +5,28 @@
 		/// <summary>
 		/// The URL to the CAPE evaluations.
 		/// </summary>
-		public static readonly string CapeUrl = "https://cape.ucsd.edu/responses/Results.aspx";
+		public const string CapeUrl = "https://cape.ucsd.edu/responses/Results.aspx";
 
 		/// <summary>
 		/// All the letters and numbers.
 		/// </summary>
-		public static readonly string LettersNumber = "ABCDEFGHIJLKMNOPQRSTUVWXYZ1234567890";
+		public const string LettersNumber = "ABCDEFGHIJLKMNOPQRSTUVWXYZ1234567890";
+
+		/// <summary>
+		/// Subjects that aren't under any departments that CAPE has, because CAPE is outdated.
+		/// </summary>
+		public static readonly string[] SubjectsToCheck =
+		{
+			"AIP",
+			"AAS",
+			"AWP",
+			"CLAS",
+			"CCS",
+			"DSC",
+			"GLBH",
+			"FMPH",
+			"SEV",
+			"SYN"
+		};
 	}
 }
--- UcsdCapeScraper/ProgramRunner.cs
+++ UcsdCapeScraper/ProgramRunner.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -13,128 +14,191 @@ using LogType = UcsdCapeScraper.Helpers.LogType;
 
 namespace UcsdCapeScraper
 {
-	public static class ProgramRunner
-	{
-		/// <summary>
-		/// Gets all CAPE data.
-		/// </summary>
-		/// <param name="driver">The driver to use.</param>
-		/// <param name="writer">The file to write to.</param>
-		public static async Task GetAllCapes(ChromeDriver driver, StreamWriter writer)
-		{
-			var stopwatch = new Stopwatch();
-			stopwatch.Start();
+    public static class ProgramRunner
+    {
+        /// <summary>
+        /// Gets all CAPE data.
+        /// </summary>
+        /// <param name="driver">The driver to use.</param>
+        /// <param name="writer">The file to write to.</param>
+        public static async Task GetAllCapes(ChromeDriver driver, StreamWriter writer)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
 
-			// get all departments
-			var departmentDropDown =
-				new SelectElement(driver.FindElement(By.Name("ctl00$ContentPlaceHolder1$ddlDepartments")));
-			var allOptions = departmentDropDown.Options
-				.Skip(1) // remove "select a department" 
-				.ToArray();
+            var processedEntries = new HashSet<string>();
 
-			var totalScraped = 0;
-			// go through each department 
-			foreach (var option in allOptions)
-			{
-				Console.WriteLine();
-				option.Click();
-				driver.FindElement(By.Name("ctl00$ContentPlaceHolder1$btnSubmit")).Click();
-				// because the website takes that long to change between
-				// the loading and not loading state 
-				await Task.Delay(TimeSpan.FromSeconds(1));
-				try
-				{
-					var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-					wait.Until(x =>
-						x.FindElement(By.Id("ContentPlaceHolder1_UpdateProgress1"))
-							.GetAttribute("style")
-							.Equals("display: none;"));
-				}
-				catch (Exception)
-				{
-					ConsoleHelper.WriteLine(LogType.Warning, $"Error getting CAPE data for {option.Text}. Skipping.");
-					continue;
-				}
+            // get all departments
+            var departmentDropDown =
+                new SelectElement(driver.FindElement(By.Name("ctl00$ContentPlaceHolder1$ddlDepartments")));
+            var totalScraped = 0;
+            // Go through each department 
+            // Note that we're skipping the "select a department" op 
+            foreach (var option in departmentDropDown.Options.Skip(1))
+            {
+                Console.WriteLine();
+                option.Click();
+                driver.FindElement(By.Name("ctl00$ContentPlaceHolder1$btnSubmit")).Click();
+                // because the website takes that long to change between
+                // the loading and not loading state 
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                if (!WaitForTableToLoad(driver, option.Text))
+                    continue;
+                ConsoleHelper.WriteLine(LogType.Info,
+                    $"CAPE website has been loaded with the department: {option.Text}");
 
-				ConsoleHelper.WriteLine(LogType.Info,
-					$"CAPE website has been loaded with the department: {option.Text}");
+                var processed = ProcessPage(driver, option.Text, processedEntries);
+                if (!processed.Any())
+                    continue;
 
-				// begin parsing 
-				var doc = new HtmlDocument();
-				doc.LoadHtml(driver.PageSource);
+                foreach (var row in processed)
+                    await writer.WriteLineAsync(row);
 
-				// get table 
-				var outerTable = doc.GetElementbyId("ContentPlaceHolder1_gvCAPEs");
-				if (outerTable is null)
-				{
-					ConsoleHelper.WriteLine(LogType.Warning, "A table could not be found for this department. " +
-					                                         "There is probably no data available. Skipping.");
-					continue;
-				}
+                ConsoleHelper.WriteLine(LogType.Info,
+                    $"Successfully scraped {processed.Count} rows for this department.");
+                totalScraped += processed.Count;
+            }
 
-				// get all the rows except for the top row 
-				var tBodyNodes = outerTable.SelectNodes("tbody");
-				if (tBodyNodes == null || tBodyNodes.Count == 0)
-				{
-					ConsoleHelper.WriteLine(LogType.Warning, $"A table was found {option.Text}, but there is " +
-					                                         "no data available. Skipping.");
-					continue;
-				}
+            // Now that we're gone through all department, we can just reset the search so we can go through 
+            // individual subjects that weren't in the department list.
+            driver.Navigate().GoToUrl(Constants.CapeUrl);
+            var searchCourseBox = driver.FindElement(By.Name("ctl00$ContentPlaceHolder1$txtCourse"));
+            foreach (var subject in Constants.SubjectsToCheck)
+            {                
+                Console.WriteLine();
+                searchCourseBox.Clear();
+                searchCourseBox.SendKeys(subject);
+                driver.FindElement(By.Name("ctl00$ContentPlaceHolder1$btnSubmit")).Click();
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                if (!WaitForTableToLoad(driver, subject))
+                    continue;
+                ConsoleHelper.WriteLine(LogType.Info,
+                    $"CAPE website has been loaded with the subject: {subject}");
 
-				var rowNodes = tBodyNodes.First().SelectNodes("tr");
+                var processed = ProcessPage(driver, subject, processedEntries);
+                if (!processed.Any())
+                    continue;
+                
+                foreach (var row in processed)
+                    await writer.WriteLineAsync(row);
 
-				var ct = 0;
-				// go through each row
-				foreach (var row in rowNodes)
-				{
-					var cells = row.SelectNodes("td");
-					var instructor = cells[0].InnerText.Trim();
-					var courseIdAndName = cells[1].ChildNodes[1].InnerText.Split("-")
-						.Select(x => x.Trim())
-						.ToArray();
+                ConsoleHelper.WriteLine(LogType.Info,
+                    $"Successfully scraped {processed.Count} rows for this subject.");
+                totalScraped += processed.Count;
+            }
 
-					// basic course info 
-					var subCourse = CleanStringInput(courseIdAndName[0]);
-					var courseName = CleanStringInput(RemoveParenLetter(courseIdAndName[1]));
-					var term = CleanStringInput(cells[2].InnerText);
-					var enrolled = CleanIntString(cells[3].InnerText);
-					var evalsMade = CleanIntString(cells[4].ChildNodes[1].InnerText);
-					// recommended class
-					var recmdClass = CleanRecommendationString(cells[5].ChildNodes[1].InnerText);
-					// recommended instructor 
-					var recmdInstructor = CleanRecommendationString(cells[6].ChildNodes[1].InnerText);
-					// study hours per week
-					var studyHrWk = CleanDecimalString(cells[7].ChildNodes[1].InnerText);
-					// avg gpa that was expected
-					var avgGradeExpected = CleanGpaString(cells[8].ChildNodes[1].InnerText);
-					// avg gpa that was received
-					var avgGradeReceived = CleanGpaString(cells[9].ChildNodes[1].InnerText);
+            await writer.FlushAsync();
+            Console.WriteLine();
+            stopwatch.Stop();
+            var timeTaken = $"{stopwatch.Elapsed.Minutes} Minutes, {stopwatch.Elapsed.Seconds} Seconds";
+            ConsoleHelper.WriteLine(LogType.Info, $"Scraped {totalScraped} rows. Time taken: {timeTaken}.");
+        }
 
-					// ReSharper disable once UseStringInterpolation
-					await writer.WriteLineAsync(string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\t{10}",
-						instructor,
-						subCourse,
-						courseName,
-						term,
-						enrolled,
-						evalsMade,
-						recmdClass,
-						recmdInstructor,
-						studyHrWk,
-						avgGradeExpected,
-						avgGradeReceived));
-					ct++;
-				}
+        /// <summary>
+        /// Waits for the table to load.
+        /// </summary>
+        /// <param name="driver">The driver.</param>
+        /// <param name="dataFor">The department or subject that we're getting data for.</param>
+        /// <returns>True if the table loaded successfully, False otherwise.</returns>
+        private static bool WaitForTableToLoad(IWebDriver driver, string dataFor)
+        {
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+                wait.Until(x =>
+                    x.FindElement(By.Id("ContentPlaceHolder1_UpdateProgress1"))
+                        .GetAttribute("style")
+                        .Equals("display: none;"));
+                return true;
+            }
+            catch (Exception)
+            {
+                ConsoleHelper.WriteLine(LogType.Warning, $"Error getting CAPE data for {dataFor}. Skipping.");
+                return false;
+            }
+        }
 
-				ConsoleHelper.WriteLine(LogType.Info, $"Successfully scraped {ct} rows for this department.");
-				totalScraped += ct;
-			}
+        /// <summary>
+        /// Processes a page from the CAPE website.
+        /// </summary>
+        /// <param name="driver">The web driver.</param>
+        /// <param name="optionText">The department or subject string.</param>
+        /// <param name="processed">The entries that are already processed.</param>
+        /// <returns>The list of entries to be written to the file.</returns>
+        private static IList<string> ProcessPage(IWebDriver driver, string optionText,
+            ISet<string> processed)
+        {
+            var entries = new List<string>();
+            var doc = new HtmlDocument();
+            doc.LoadHtml(driver.PageSource);
 
-			await writer.FlushAsync();
-			Console.WriteLine();
-			stopwatch.Stop();
-			var timeTaken = $"{stopwatch.Elapsed.Minutes} Minutes, {stopwatch.Elapsed.Seconds} Seconds";
-			ConsoleHelper.WriteLine(LogType.Info, $"Scraped {totalScraped} rows. Time taken: {timeTaken}.");
-		}
-	}
+            // get table 
+            var outerTable = doc.GetElementbyId("ContentPlaceHolder1_gvCAPEs");
+            if (outerTable is null)
+            {
+                ConsoleHelper.WriteLine(LogType.Warning, "A table could not be found for this department/subject. " +
+                                                         "There is probably no data available. Skipping.");
+                return entries;
+            }
+
+            // get all the rows except for the top row 
+            var tBodyNodes = outerTable.SelectNodes("tbody");
+            if (tBodyNodes == null || tBodyNodes.Count == 0)
+            {
+                ConsoleHelper.WriteLine(LogType.Warning, $"A table was found for {optionText}, but there is " +
+                                                         "no data available. Skipping.");
+                return entries;
+            }
+
+            var rowNodes = tBodyNodes.First().SelectNodes("tr");
+
+            // go through each row
+            foreach (var row in rowNodes)
+            {
+                var cells = row.SelectNodes("td");
+                var instructor = cells[0].InnerText.Trim();
+                var courseIdAndName = cells[1].ChildNodes[1].InnerText.Split("-")
+                    .Select(x => x.Trim())
+                    .ToArray();
+
+                // basic course info 
+                var subCourse = CleanStringInput(courseIdAndName[0]);
+                var courseName = CleanStringInput(RemoveParenLetter(courseIdAndName[1]));
+                var term = CleanStringInput(cells[2].InnerText);
+                var enrolled = CleanIntString(cells[3].InnerText);
+                var evalsMade = CleanIntString(cells[4].ChildNodes[1].InnerText);
+                // recommended class
+                var recmdClass = CleanRecommendationString(cells[5].ChildNodes[1].InnerText);
+                // recommended instructor 
+                var recmdInstructor = CleanRecommendationString(cells[6].ChildNodes[1].InnerText);
+                // study hours per week
+                var studyHrWk = CleanDecimalString(cells[7].ChildNodes[1].InnerText);
+                // avg gpa that was expected
+                var avgGradeExpected = CleanGpaString(cells[8].ChildNodes[1].InnerText);
+                // avg gpa that was received
+                var avgGradeReceived = CleanGpaString(cells[9].ChildNodes[1].InnerText);
+
+                // ReSharper disable once UseStringInterpolation
+                var rowText = string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\t{10}",
+                    instructor,
+                    subCourse,
+                    courseName,
+                    term,
+                    enrolled,
+                    evalsMade,
+                    recmdClass,
+                    recmdInstructor,
+                    studyHrWk,
+                    avgGradeExpected,
+                    avgGradeReceived);
+                if (processed.Contains(rowText))
+                    continue;
+
+                entries.Add(rowText);
+                processed.Add(rowText);
+            }
+
+            return entries;
+        }
+    }
 }
